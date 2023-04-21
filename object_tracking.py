29eaa5a4554c58a11d41ac10455fa0509fcfba34
ManbirK <72926520+ManbirK@users.noreply.github.com>
2023-04-21 18:46:07 +0530

    Update object_tracking.py

--- object_tracking.py
+++ object_tracking.py
@@ -3,88 +3,95 @@
 import cv2
 import time
 import math
+  
 
-p1 = 530
-p2 = 300
+basket_c_x = 500
 
-xs = []
-ys = []
+basket_c_y = 300
+
+
+trajectory_x = []
+trajectory_y = []
 
 video = cv2.VideoCapture("bb3.mp4")
-# Load tracker 
+
+
+#tracker load
+
 tracker = cv2.TrackerCSRT_create()
 
-# Read the first frame of the video
+
+# load the first frame of the video
 returned, img = video.read()
 
-# Select the bounding box on the image
-bbox = cv2.selectROI("Tracking", img, False)
+#Select the bounding box on the image
+#cv2.selectROI("Name of the region of interest", Image on which we want to create it )
+ball_box = cv2.selectROI("Tracking", img, False)
+#ballbox = [x,y,w,h]
 
-# Initialise the tracker on the img and the bounding box
-tracker.init(img, bbox)
 
-def drawBox(img, bbox):
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+#iniialize the tracker
+
+tracker.init(img,ball_box)
+print(ball_box)
+
+
+# draw_box
+
+def drawBox(img,ball_box):
+
+    x,y,w,h = int(ball_box[0]),int(ball_box[1]),int(ball_box[2]),int(ball_box[3])
 
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
 
     cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+ 
+#goal track
+def goal_track(img,ball_box):
+    x,y,w,h = int(ball_box[0]),int(ball_box[1]),int(ball_box[2]),int(ball_box[3])
 
+    c_x = x + int(w/2)
 
-def goal_track(img, bbox):
-    
-    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    c_y = y + int(h/2)
 
-    # Get the CENTER Points of the Bounding Box
-    c1 = x + int(w/2)
-    c2 = y + int(h/2)
+    cv2.circle(img,(c_x,c_y),2, (0,255,0),4)
 
-    # Draw a small circle using CENTER POINTS
-    cv2.circle(img,(c1,c2),2,(0,0,255),5)
+    cv2.circle(img,(int(basket_c_x),int(basket_c_y)),2, (0,255,0),4)
 
-    cv2.circle(img,(int(p1),int(p2)),2,(0,255,0),3)
-
-    # Calculate Distance
-    dist = math.sqrt(((c1-p1)**2) + (c2-p2)**2)
+    dist = math.sqrt(((c_x-basket_c_x)**2 ) + ((c_y-basket_c_y)**2 ))
     print(dist)
 
-    # Goal is reached if distance is less than 20 pixel points
-    if(dist<=20):
-        cv2.putText(img,"Goal",(300,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+    if(dist<=25):
+        cv2.putText(img,"Goal",(250,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
-    xs.append(c1)
-    ys.append(c2)
+    trajectory_x.append(c_x)
+    trajectory_y.append(c_y)   
 
-    for i in range(len(xs)-1):
-        cv2.circle(img,(xs[i],ys[i]),2,(0,0,255),5)
+    for pos in range(len(trajectory_x)-1):
+        cv2.circle(img,(trajectory_x[pos],trajectory_y[pos]),2, (0,255,0),4)
 
+     
 while True:
-    
-    check, img = video.read()   
+    check,img = video.read()   
 
-    # Update the tracker on the img and the bounding box
-    success, bbox = tracker.update(img)
+    success, ball_box = tracker.update(img)
 
-    # Call drawBox()
     if success:
-        drawBox(img, bbox)
+        drawBox(img,ball_box)
     else:
-        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
-    # Call goal_track()
-    goal_track(img, bbox)
+    goal_track(img,ball_box)
 
-    # Display Video
-    cv2.imshow("result", img)
-
-
-    # Quit Display Window when Spacebar key is pressed        
+    cv2.imshow("result",img)
+            
     key = cv2.waitKey(25)
+
     if key == 32:
-        print("Stopped")
+        print("Stopped!")
         break
+
 
 video.release()
 cv2.destroyALLwindows()
-
 
